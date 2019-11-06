183ec61cbc48ac321da395d6f052ece9f1679590
Enzo Busseti <enzo.busseti@me.com>
2019-11-06 23:48:37 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='greedy_grid_search',
+      description='Greedy grid search for hyper-parameter optimization.',
+      author='Enzo Busseti',
+      license='GPLv3',
+      version='0.1'
+      )
